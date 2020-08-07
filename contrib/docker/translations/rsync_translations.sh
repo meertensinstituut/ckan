7153ba333f9ab897b588188a87743c30a47b5ba2
Vic Ding <qiqing.ding@meertens.knaw.nl>
2020-08-07 13:31:56 +0200

    translate all 3 langauges

--- contrib/docker/translations/rsync_translations.sh
+++ contrib/docker/translations/rsync_translations.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rsync -avzhe ssh ucla:/srv/isebel/translations/dan_eng .
 rsync -avzhe ssh ucla:/srv/isebel/translations/nld_eng .
+rsync -avzhe ssh ucla:/srv/isebel/translations/deu_eng .
