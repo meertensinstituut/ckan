7beb4c93cf94fbe4277ad3e662d407cd93f97998
Vic Ding <qiqing.ding@meertens.knaw.nl>
2020-12-13 19:31:20 +0100

    sync as well Fry translations

--- contrib/docker/translations/rsync_translations.sh
+++ contrib/docker/translations/rsync_translations.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
 rsync -avzhe ssh ucla:/srv/isebel/translations/dan_eng .
 rsync -avzhe ssh ucla:/srv/isebel/translations/nld_eng .
+rsync -avzhe ssh ucla:/srv/isebel/translations/fry_eng .
 rsync -avzhe ssh ucla:/srv/isebel/translations/deu_eng .
